b1b180775172e768dd8696e8d897dc76f9906c56
Kushal Paliwal <kushal5paliwal@gmail.com>
2026-08-08 15:48:17 +0530

    x

--- components/ScoreSubmitVaraPaymentModal.tsx
+++ components/ScoreSubmitVaraPaymentModal.tsx
@@ -190,8 +190,14 @@ export default function ScoreSubmitVaraPaymentModal({
       return;
     }
 
+    if (!programConfigured) {
+      setError(
+        "Score submit program is not configured. Redeploy with NEXT_PUBLIC_VARA_SCORE_SUBMIT_PROGRAM."
+      );
+    }
+
     void loadBalances();
-  }, [open, loadBalances]);
+  }, [open, loadBalances, programConfigured]);
 
   const handlePay = useCallback(
     async (token?: VaraShopPaymentToken) => {
